198e5817537a5009b989b75d390f7e922639cec6
marcmamaradlo <marc.edwin.mamaradlo@gmail.com>
2023-10-27 05:31:34 +0800

    Fixes

--- utils/index.ts
+++ utils/index.ts
@@ -7,7 +7,10 @@ export async function fetchCars(filters: FilterProps) {
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
 
-    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model${model}&limit=${limit}&fuel_type${fuel}`, {headers: headers,});
+    const response = await fetch(
+      `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model${model}&limit=${limit}&fuel_type${fuel}`, 
+    {headers: headers,}
+    );
     const result = await response.json();
     return result;
 }
